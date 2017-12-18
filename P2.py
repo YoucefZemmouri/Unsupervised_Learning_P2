1bf84f4cf8ff5560d4c740a5e1b98ba8b34b77c3
Leman FENG <flm8620@gmail.com>
2017-12-18 23:53:31 +0100

    SSC ready

--- P2.py
+++ P2.py
@@ -28,6 +28,7 @@ def clustering_error(clus, labels, n):
     correctly_classified_samples = A[row_ind, col_ind].sum()
     return 1-correctly_classified_samples/N
 
+
 def show_error_table(cells, sigmas, Ks):
     cells = cells.round(3)
     Ks = [str(x)+'-NN' for x in Ks]
@@ -36,24 +37,22 @@ def show_error_table(cells, sigmas, Ks):
     ax.axis('tight')
     ax.axis('off')
     the_table = ax.table(cellText=cells,
-                          rowLabels=Ks,
-                          colLabels=sigmas,
-                          loc='center',
+                         rowLabels=Ks,
+                         colLabels=sigmas,
+                         loc='center',
                          cellColours=plt.cm.cool(cells))
-    # sm = plt.cm.ScalarMappable(cmap=plt.cm.cool, norm=plt.Normalize(vmin=0, vmax=1))
-    # sm._A = []
-    # plt.colorbar(sm, fraction=0.026, pad=0.04)
+    plt.title('Clustering error with choice of sigma and k-NN')
     plt.show()
+
 
 def find_best_K_and_sigma(X_small, labels_small, n):
     K_largest = 30
     nbrs = NearestNeighbors(n_neighbors=K_largest, algorithm='ball_tree').fit(X_small.T)
 
-    K_ranges = np.arange(2, 10, 1)
-    sigma_ranges = np.array([0.25, 0.5, 1, 1.5, 2, 2.5, 3, 4, 5])
+    K_ranges = np.arange(2, 20, 1)
+    sigma_ranges = np.array([0.25, 0.5, 1, 1.5, 2, 2.5, 3, 4, 5, 7, 9, 13, 20, 30])
 
-    Kcoord, sigma_coord = np.meshgrid(K_ranges, sigma_ranges, indexing='ij')
-    errors = np.zeros(Kcoord.shape)
+    errors = np.zeros((len(K_ranges), len(sigma_ranges)))
     for ki in range(len(K_ranges)):
         K = K_ranges[ki]
         distances, indices = nbrs.kneighbors(X_small.T, K)
@@ -68,7 +67,7 @@ def find_best_K_and_sigma(X_small, labels_small, n):
                     w = np.exp(- dist ** 2 / 2 / sq_sigma)
                     W[i, index] = W[index, i] = w
 
-            clus, _ = spectralClustering(W, n)
+            clus, _ = SpectralClustering(W, n)
             error = clustering_error(clus, labels_small, n)
             errors[ki, sigma_i] = error
 
@@ -82,14 +81,29 @@ X = mat['EYALEB_DATA'] / 255.  # rescale data to [0,1]
 labels = mat['EYALEB_LABEL'].flatten()
 labels -= 1  # to make range in {0, 1, ..., n-1}
 
-illum_num = 64 # number of faces for each person
+im_shape = (42, 48)
+illum_num = 64  # number of faces for each person
 
 n = 2
 X_small = X[:, 0:illum_num*n]
 labels_small = labels[0:illum_num*n]
 
+# show one image
+# plt.imshow(X[:, 2].reshape(im_shape).T)
+# plt.colorbar()
+# plt.show()
+
+# SSC
+clus, _ = SSC(X_small, n, tau=0.05, mu2=1000, epsilon=0.0000001)
+error = clustering_error(clus, labels_small, n)
+print('SSC, clustering error = {:.2f}%'.format(error*100))
+
+# SpectralClustering
 find_best_K_and_sigma(X_small, labels_small, n)
 
-clus, _, _ = K_Subspaces(X_small, n, [3,3])  # 3 dimension subspace for face
-error = clustering_error(clus, labels_small, n)
-print(error)
+# K subspace
+for i in range(5):  # multiple trials for K-subspace
+    di = [3,3]  # 3 dimension subspace for faces
+    clus, sq_dist, _, _ = K_Subspaces(X_small, n, di, restart=1)
+    error = clustering_error(clus, labels_small, n)
+    print('clustering error = {:.2f}%'.format(error*100))
--- Tools.py
+++ Tools.py
@@ -1,10 +1,11 @@
+from __future__ import print_function
 import numpy as np
 from sklearn.cluster import KMeans
 from numpy import linalg as LA
 from scipy import linalg as sLA
 import time
 from scipy.stats import ortho_group
-
+from sklearn.decomposition import PCA
 
 def SoftThreshold(epsilon, x):
     """
@@ -15,8 +16,7 @@ def SoftThreshold(epsilon, x):
     return np.sign(x) * np.maximum(np.abs(x) - epsilon, 0)
 
 
-# Spectral Clustering
-def spectralClustering(W, n):
+def SpectralClustering(W, n):
     """
     Spectral Clustering (Algorithm 4.5)
     :param W: Affinity matrix
@@ -26,13 +26,13 @@ def spectralClustering(W, n):
     N = W.shape[0]
     L = np.diag(W.dot(np.ones(N))) - W
     evalue, evector = LA.eigh(L)
-    Y = evector[:,0:n]
+    Y = evector[:, 0:n]
     Clus = KMeans(n_clusters=n, random_state=0).fit(Y)
     return Clus.labels_, Y
 
 
 # Sparse Subspace Clustering for Noisy Data
-def LASSO(X, mu2=0.1, tau=0.1, epsilon=0.1):
+def LASSO(X, mu2=0.1, tau=0.1, epsilon=0.01):
     """
     Matrix LASSO Minimization by ADMM (Algorithm 8.5)
     :param X: Data matrix
@@ -40,25 +40,28 @@ def LASSO(X, mu2=0.1, tau=0.1, epsilon=0.1):
     :param tau: step gradient ascent
     :return: Sparse representation C
     """
-    C = np.zeros(X.shape)
-    L = np.zeros(X.shape)
-    I = np.identity(X.shape[1])
-    A = np.linalg.inv(tau * (X.T).dot(X) + mu2 * I)
-    XtX = tau * (X.T).dot(X)
+    N = X.shape[1]
+    D = X.shape[0]
+    C = np.zeros((N, N))
+    L = np.zeros((N, N))
+    I = np.identity(N)
+    tauXtX = tau * (X.T).dot(X)
+    A = np.linalg.inv(tauXtX + mu2 * I)
 
     count = 0
     t = time.time()
-
     while True:
-        Z = A.dot(XtX + mu2 * C - L)
+        Z = A.dot(tauXtX + mu2 * C - L)
         C = SoftThreshold(1 / mu2, Z + L / mu2)
-        C = C - np.diag(np.diag(C))
+        np.fill_diagonal(C, 0)
+        # C = C - np.diag(np.diag(C))
         L = L + mu2 * (Z - C)
-        max_norm = np.max(np.abs(Z))
+        max_norm = np.max(np.abs(Z - C))
         if count % 10 == 0:
             print('i=', count, ' step = ', max_norm)
         if max_norm < epsilon:
             break
+        old_C=C
         count += 1
     elapsed = time.time() - t
     print(count, ' iterations used, ', elapsed, ' seconds')
@@ -73,17 +76,22 @@ def NormalizedSpectralClustering(n, W):
     :return: Segmentation of the data in n groups
     """
     N = W.shape[0]
-    D = np.diag(W.dot(np.ones(N)))
-    D_12 = np.linalg.inv(np.sqrt(D))
+    D = W.dot(np.ones(N))
+    if np.any(D == 0):
+        print("Warning: at least one sample is completely isolated from others")
+    D_12 = np.sqrt(D)  # D is diagonal, inv(D) = 1/D
+    D_12[D_12 != 0] = 1/D_12[D_12 != 0]
+    D = np.diag(D)
+    D_12 = np.diag(D_12)
     L = D - W
     M = D_12.dot(L).dot(D_12)
     evalue, evector = LA.eig(M)
-    Y = np.matrix(evector[0:n])
-    Clus = KMeans(n_clusters=n, random_state=0).fit(Y.T)
-    return Clus.labels_, Y.T
+    Y = evector[:, 0:n]
+    Clus = KMeans(n_clusters=n, random_state=0).fit(Y)
+    return Clus.labels_, Y
 
 
-def SSC(X, n, mu2=0.1, tau=0.1):
+def SSC(X, n, mu2=0.1, tau=0.1, epsilon=0.01):
     """
     SSC for Noisy Data (Algorithm 8.6)
     :param X: Data
@@ -92,75 +100,92 @@ def SSC(X, n, mu2=0.1, tau=0.1):
     :param tau: step gradient ascent
     :return: Segmentation of the data into n groups
     """
-    C = LASSO(X, mu2, tau)
+    C = LASSO(X, mu2, tau, epsilon)
     W = np.abs(C) + (np.abs(C)).T
     return NormalizedSpectralClustering(n, W)
 
 
-# K-Subspaces
-def K_Subspaces(X, n, d):
+def K_Subspaces(X, n, d, restart):
     """
     K-Subspaces
     :param X: Data
     :param n: Number of subspaces
     :param d: List of dimensions of the subspaces
+    :param restart: number of restarts
     :return: assignments, subspaces represented by U & mu
     """
     print('K-subspace...')
     assert(len(d) == n)
     N = X.shape[1]
     D = X.shape[0]
-    Mus = np.random.rand(D, n)
-    U = [np.array([]) for _ in range(n)]
-    # generate random ortho matrix is very slow for large dimension
-    # for l in range(n):
-    #     print(1)
-    #     temp = special_ortho_group.rvs(D)
-    #     temp = temp[:, 0:d[l]]
-    #     U.append(temp)
+    minimum_sum_sq_dist = 1e10
 
-    # subspace_has[l] is the list of point indices that belongs to Subspace_l
-    subspace_has = [[] for _ in range(n)]
+    for r in range(restart):
+        print('Repeat {}: '.format(r), end='')
+        Mus = np.zeros((D, n))
+        U = [np.array([]) for _ in range(n)]
 
-    # start with a random assignment
-    for j in range(N):
-        subspace_has[np.random.randint(n)].append(j)
+        # generate random ortho matrix is very slow for large dimension.
+        # instead, we want to initialize by random assignments to subspaces
+        # for l in range(n):
+        #     print(1)
+        #     temp = ortho_group.rvs(D) # so long...
+        #     temp = temp[:, 0:d[l]]
+        #     U.append(temp)
 
-    subspace_has_old = subspace_has
-    count = 0
-    while True:
-        print(count)
-        # Do PCA for each subspace
-        for l in range(n):
-            points = subspace_has[l]
-            X_in_l = X[:, points]
-            Mus[:, l] = X_in_l.sum(1).T / len(points)
-            x_mu = X_in_l - Mus[:, l, None]
-            M = x_mu.dot(x_mu.T)
-            evalue, evector = sLA.eigh(M,eigvals=(D-d[l],D-1))  # d[l] largest eigen
-            U[l] = evector # last(largest) d[l] evector
-
-        # Calculate distance
-        I = np.identity(D)
-        dist = np.zeros((n, N))  # dist[l,j] means distance of X_j to Subspace_l
-        for l in range(n):
-            A = (I - U[l].dot(U[l].T))
-            for j in range(N):
-                v = A.dot(X[:, j]-Mus[:, l])
-                dist[l, j] = np.sum(v*v)
-
-        # Assign point to nearest subspace
+        # subspace_has[l] is the list of point indices that belongs to Subspace_l
         subspace_has = [[] for _ in range(n)]
+        # start with a random assignment
         for j in range(N):
-            l_best_for_j = np.argmin(dist[:, j])
-            subspace_has[l_best_for_j].append(j)
-        if subspace_has_old == subspace_has:  # no updates for assignments
-            break
+            subspace_has[np.random.randint(n)].append(j)
         subspace_has_old = subspace_has
-        count += 1
 
-    assignments = np.zeros(N).astype(int)  # assignments[j] means X_j assigned to Subspace_{assignments[j]}
-    for l in range(n):
-        assignments[subspace_has[l]] = l
-    print('{} iterations used for K-subspace'.format(count))
-    return assignments, U, Mus
+        count = 0
+        while True:
+            print(str(count)+' ', end='')
+            count += 1
+            # Do PCA for each subspace
+            for l in range(n):
+                points = subspace_has[l]
+                X_in_l = X[:, points]
+                Mus[:, l] = X_in_l.sum(1).T / len(points)
+
+                # eigen decomposition in sLA is now fast enough
+                # x_mu = X_in_l - Mus[:, l, None]
+                # M = x_mu.dot(x_mu.T)
+                # evalue, evector = sLA.eigh(M,eigvals=(D-d[l],D-1))  # d[l] largest eigen
+                # U[l] = evector  # last(largest) d[l] evector
+
+                # so instead, we use a optimized PCA module in sklearn
+                pca = PCA(n_components=d[l])
+                pca.fit(X_in_l.T)
+                U[l] = pca.components_.T
+
+            # Calculate distance
+            I = np.identity(D)
+            sqdist = np.zeros((n, N))  # dist[l,j] means distance of X_j to Subspace_l
+            for l in range(n):
+                A = (I - U[l].dot(U[l].T))
+                for j in range(N):
+                    v = A.dot(X[:, j]-Mus[:, l])
+                    sqdist[l, j] = np.sum(v*v)
+
+            # Assign point to nearest subspace
+            subspace_has = [[] for _ in range(n)]
+            for j in range(N):
+                l_best_for_j = np.argmin(sqdist[:, j])
+                subspace_has[l_best_for_j].append(j)
+            if subspace_has_old == subspace_has:  # stop when no updates for assignments
+                break
+            subspace_has_old = subspace_has
+
+        sum_sq_dist = sqdist.sum()
+        print('sum_sq_dist = {}'.format(sum_sq_dist))
+        if sum_sq_dist < minimum_sum_sq_dist:
+            minimum_sum_sq_dist = sum_sq_dist
+            best_assignments = np.zeros(N).astype(int)  # assignments[j] means X_j assigned to Subspace_{assignments[j]}
+            for l in range(n):
+                best_assignments[subspace_has[l]] = l
+
+    print('Sum of sq distance = {}'.format(minimum_sum_sq_dist))
+    return best_assignments, minimum_sum_sq_dist, U, Mus
