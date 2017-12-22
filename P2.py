929fc25a95433d042f7183e906db085f5bd6e7db
Leman FENG <flm8620@gmail.com>
2017-12-22 19:56:58 +0100

    add some plot in P2 for visualization of Ksubspace result

--- P2.py
+++ P2.py
@@ -33,7 +33,7 @@ illum_num = 64  # number of faces for each person
 
 
 # select n first individuals
-n = 10
+n = 2
 selections = []
 for i in range(n):
     for j in range(len(labels_global)):
@@ -46,21 +46,47 @@ labels = labels_global[selections]
 print('{} groups, {} faces in total'.format(n,X.shape[1]))
 
 
-# SpectralClustering
+# ------ SpectralClustering ------
 find_best_K_and_sigma_for_SC(X, labels, n)
 # W = affinity(X, K=4, sigma=4.0)
 # clus, _ = SpectralClustering(W, n)
 # e = clustering_error(clus, labels, n);
 # print('SC clustering error = {:.2f}%, K={},s={}'.format(e*100,4,4.0))
 
-# K subspace
-for i in range(5):  # multiple trials for K-subspace
-    di = [3]*n  # 3 dimension subspace for faces
-    clus, sq_dist, _, _ = K_Subspaces(X, n, di, restart=1)
-    error = clustering_error(clus, labels, n)
-    print('K-subspace clustering error = {:.2f}%'.format(error*100))
 
-# SSC
+# ------ K subspace ------
+errors = []
+sq_distance = []
+best_errors = []
+trials = 30
+trial_length = 10
+for k in range(trials):
+    best_errors.append([])
+    best = 1e10
+    for i in range(trial_length):  # multiple trials for K-subspace
+        di = [3]*n  # 3 dimension subspace for faces
+        clus, sq_dist, _, _ = K_Subspaces(X, n, di, restart=1)
+        error = clustering_error(clus, labels, n)
+        print('K-subspace clustering error = {:.2f}%'.format(error*100))
+        errors.append(error)
+        sq_distance.append(sq_dist)
+        best = min(best, error)
+        best_errors[-1].append(best)
+plt.figure()
+for k in range(trials):
+    plt.plot(np.arange(1,trial_length+1,1), best_errors[k], label='trial '+str(k))
+plt.xlabel('Number of restart')
+plt.ylabel('Minimum error so far')
+plt.show()
+
+plt.figure()
+plt.scatter(errors, sq_distance)
+plt.xlabel('Clustering Error')
+plt.ylabel('Sum of Sqaured Distance of point to nearest plane')
+plt.show()
+
+
+# ------  SSC  ------ 
 clus, _ = SSC(X, n, tau=0.05, mu2=1000, epsilon=0.0000001)
 error = clustering_error(clus, labels, n)
 print('SSC, clustering error = {:.2f}%'.format(error*100))
