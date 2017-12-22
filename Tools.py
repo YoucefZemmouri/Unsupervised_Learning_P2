7de768b0d4ac3339b5c47d8767881e80444dec9c
martintsd <m.troussard35@gmail.com>
2017-12-22 20:02:18 +0100

    Verbose

--- Tools.py
+++ Tools.py
@@ -107,7 +107,7 @@ def SSC(X, n, mu2=0.1, tau=0.1, epsilon=0.01, verbose=True):
     return NormalizedSpectralClustering(n, W)
 
 
-def K_Subspaces(X, n, d, restart):
+def K_Subspaces(X, n, d, restart, verbose=True):
     """
     K-Subspaces
     :param X: Data
@@ -116,14 +116,16 @@ def K_Subspaces(X, n, d, restart):
     :param restart: number of restarts
     :return: assignments, subspaces represented by U & mu
     """
-    print('K-subspace...')
+    if verbose:
+        print('K-subspace...')
     assert(len(d) == n)
     N = X.shape[1]
     D = X.shape[0]
     minimum_sum_sq_dist = 1e10
 
     for r in range(restart):
-        print('Repeat {}: '.format(r), end='')
+        if verbose:
+            print('Repeat {}: '.format(r), end='')
         Mus = np.zeros((D, n))
         U = [np.array([]) for _ in range(n)]
 
@@ -144,7 +146,8 @@ def K_Subspaces(X, n, d, restart):
 
         count = 0
         while True:
-            print(str(count)+' ', end='')
+            if verbose:
+                print(str(count)+' ', end='')
             count += 1
             # Do PCA for each subspace
             for l in range(n):
@@ -185,14 +188,16 @@ def K_Subspaces(X, n, d, restart):
                 break
             subspace_has_old = subspace_has
 
-        print('sum_sq_dist = {}'.format(sum_sq_dist))
+        if verbose:
+            print('sum_sq_dist = {}'.format(sum_sq_dist))
         if sum_sq_dist < minimum_sum_sq_dist:
             minimum_sum_sq_dist = sum_sq_dist
             best_assignments = np.zeros(N).astype(int)  # assignments[j] means X_j assigned to Subspace_{assignments[j]}
             for l in range(n):
                 best_assignments[subspace_has[l]] = l
 
-    print('Sum of sq distance = {}'.format(minimum_sum_sq_dist))
+    if verbose:
+        print('Sum of sq distance = {}'.format(minimum_sum_sq_dist))
     return best_assignments, minimum_sum_sq_dist, U, Mus
 
 
